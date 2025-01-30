9765902bbf114501d6a72233f45997fe13d27f50
brett <brett@DonLish>
2025-01-29 20:52:49 -0500

    implemented logic to add 2 variables

--- src/demo1/DemoTest5.java
+++ src/demo1/DemoTest5.java
@@ -6,6 +6,13 @@ public static void main(String[] args) {
 		
 		System.out.println("Java");
 		
+		int a = 10;
+		int b = 20;
+		
+		int result = a + b;
+		
+		System.out.println(result);
+		
 	}
 
 }
